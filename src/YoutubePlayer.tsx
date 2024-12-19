24106969be20dd5e03b9c4cbb470569824b6aa9b
mji4209 <mji4209@naver.com>
2024-12-19 13:09:23 +0900

    재생 속도, 일정 초 넘기기 기능 추가

--- src/YoutubePlayer.tsx
+++ src/YoutubePlayer.tsx
@@ -5,13 +5,12 @@ interface Clip{
   startTime: string,
   endTime: string,
   korSub: string,
-  // engSub: string
 }
 
 const YoutubePlayer: React.FC = () => {
   const playerRef = useRef<HTMLDivElement>(null);
 
-  const [url, setUrl] = useState<string>('');
+  const [videoId, setVideoId] = useState<string>('');
   const [inputValue, setInputValue] = useState<string>('');
 
   const [player, setPlayer] = useState<YT.Player | null>(null);
@@ -19,6 +18,8 @@ const YoutubePlayer: React.FC = () => {
   const inputValueHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
   }
+
+  //Youtube Player 코드
 
   useEffect(() => {
     const script = document.createElement("script");
@@ -31,7 +32,7 @@ const YoutubePlayer: React.FC = () => {
         const newPlayer = new window.YT.Player(playerRef.current, {
           height: '390',
           width: '640',
-          videoId: url,
+          videoId: videoId,
           playerVars: {
             autoplay: 0,
             controls: 1,
@@ -49,60 +50,14 @@ const YoutubePlayer: React.FC = () => {
   }, []);
 
   useEffect(() => {
-    if (player && url) {
-      player.loadVideoById(url);
+    if (player && videoId) {
+      player.loadVideoById(videoId);
+      player.pauseVideo();
     }
-  }, [url, player]);
+  }, [videoId]);
 
+  // 재생위치 설정 코드
   
-  const [file, setFile] = useState<File | null>(null);
-  const [data, setData] = useState < Clip[]>([]);
-
-  const fileHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) {
-      setFile(e.target.files[0]);
-      fileReader(e.target.files[0]);
-    }
-  }
-
-  const hmsToNumber = (time: string) => {
-    if (time) { 
-      const [hours, minutes, seconds, zpo] = time.split(":").map(Number);
-      return hours * 3600 + minutes * 60 + seconds + zpo * 0.01;
-    } else {
-      return 0;
-    }
-  }
-
-  const fileReader = (file: File) => {
-    const reader = new FileReader();
-
-    reader.onload = (event) => {
-      const text = event.target?.result as string;
-      const rows = text.split("\n").map((row) => row.split(','));
-      console.log('csv 데이터 : ', rows);
-      const arr: Clip[] = [];
-      for (let i = 1; i < rows.length; i++){
-        const content: Clip = {
-          speakerName : rows[i][0],
-          startTime : rows[i][1],
-          endTime : rows[i][2],
-          korSub : rows[i][3],
-          // engSub : rows[i].slice(4).join('')
-        }
-        arr.push(content);
-      }
-      setData(arr);
-      console.log(arr);
-      console.log(data);
-    }
-    reader.readAsText(file);
-  }
-
-  useEffect(() => {
-    console.log(`data update: ${JSON.stringify(data, null, 2)}`)
-  }, [data])
-
   const onPlayLoop = (start: string, end: string) => {
     const startTime = hmsToNumber(start);
     const endTiem = hmsToNumber(end);
@@ -117,25 +72,79 @@ const YoutubePlayer: React.FC = () => {
     }, 10)
   }
 
+  // 재생시간 설정코드(hh:mm:ss:ss string -> number)
 
-    
+  const hmsToNumber = (time: string) => {
+    if (time) { 
+      const [hours, minutes, seconds, zpo] = time.split(":").map(Number);
+      return hours * 3600 + minutes * 60 + seconds + zpo * 0.01;
+    } else {
+      return 0;
+    }
+  }
+
+  
+  // csv파일 업로드 및 분석 코드
+  
+  const [file, setFile] = useState<File | null>(null);
+  const [data, setData] = useState < Clip[]>([]);
+
+  const fileHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setFile(e.target.files[0]);
+      fileReader(e.target.files[0]);
+    }
+  }
+
+
+  const fileReader = (file: File) => {
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      const text = event.target?.result as string;
+      const rows = text.split("\n").map((row) => row.split(','));
+      const arr: Clip[] = [];
+      for (let i = 1; i < rows.length; i++){
+        const content: Clip = {
+          speakerName : rows[i][0],
+          startTime : rows[i][1],
+          endTime : rows[i][2],
+          korSub : rows[i][3],
+        }
+        arr.push(content);
+      }
+      setData(arr);
+    }
+    reader.readAsText(file);
+  }
+
+  // useEffect(() => {
+  //   console.log(`data update: ${JSON.stringify(data, null, 2)}`)
+  // }, [data])
+
+  
+
+  
   return (
       <div>
-      {/* <YouTube videoId="crLbUTFh2oQ" opts={opts} onReady={onPlayerReady} /> */}
+        {/* sample video id : crLbUTFh2oQ */}
       <div ref={playerRef}/>
-      <input type="file" accept=".csv" onChange={fileHandler} />
       <input type="text" value={inputValue} onChange={inputValueHandler} />
       <button onClick={() => {
-        setUrl(inputValue) 
-        console.log(url)
-      }}>seturl</button>
-      
+        setVideoId(inputValue) 
+      }}>setVideoId</button>
+      <button onClick={() => {
+        player?.setPlaybackRate(0.5)
+      }}>0.5</button>
+      <button onClick={() => {
+        player?.seekTo(player.getCurrentTime() + 0.01, true)
+      }}>+0.01</button>
+      <input type="file" accept=".csv" onChange={fileHandler} />
       {file && <p>선택된 파일: {file.name}</p>}
         <div>
           {data.map((item, index) => (
             <div key={index}>
               {item.startTime} ~ {item.endTime} <br /> kor : {item.korSub} <br/>
-              {/* <br /> eng : {item.engSub} */}
               <button onClick={() => onPlayLoop(item.startTime, item.endTime)}>start</button>
             </div>
           ))}
